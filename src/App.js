b8260f8e477e0256aea107de7fae08f67681d0ab
Andres Felipe Diago Matta <andresdiag@unicauca.edu.co>
2024-09-15 15:17:17 -0500

    :sparkles: Nuevo componente

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import logo from './platzi.webp';
 
 function App() {
+  // This is a functional component
   return (
+    //This is not HTML, this is JSX, js with xml syntax
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +20,30 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        <h1>Todo List</h1>
+        <ul>
+          <TodoItem count={3} />
+          <TodoItem count={1} />
+          <TodoItem count={5} />
+        </ul>
+      </section>
     </div>
+  );
+}
+
+// new component
+function TodoItem(props) {
+  return (
+    <li>
+      <h1>Task</h1>
+      <span>V</span>
+      <p>
+        Completaste {props.count} tareas de 5. Hora:{' '}
+        {new Date().toLocaleTimeString()}
+      </p>
+      <span></span>
+    </li>
   );
 }
 
