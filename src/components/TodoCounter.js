5546b0c28481550099010bd81e5b6a1df662935b
Andres Felipe Diago Matta <andresdiag@unicauca.edu.co>
2024-09-29 21:10:49 -0500

    :construction: Usando estados

--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import '../assets/styles/TodoCounter.css';
 
 // new component
 function TodoCounter(props) {
+	const [searchValue, setSearchValue] = React.useState('');
+	/*
+	 * The state in react is a variable that holds the value of the component, and it can be changed by the component itself.
+	 * The state is inmutable, so it can't be changed directly, it has to be changed by the method setState.
+	 */
+
+	console.log('Los usuarios han completado: ', searchValue);
+  // console.log is executed every time the component is rendered
+
 	return (
 		<>
 			<h1 className='todo-counter'>
@@ -11,9 +21,13 @@ function TodoCounter(props) {
 				<input
 					placeholder='Search...'
 					className='search-input'
+					value={searchValue}
 					onChange={event => {
-						console.log(event.target.value);
+						// console.log(event.target.value);
+						setSearchValue(event.target.value);
 					}}
+					type='text'
+					aria-label='Search Todos'
 				/>
 				<span className='search-icon'>&#128269;</span>
 			</div>
