6e4d7d7f3b3ac83b737251d7ac8e22bc9b0c2fbd
Andres Felipe Diago Matta <andresdiag@unicauca.edu.co>
2024-10-14 21:13:40 -0500

    :construction: Usando LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Confetti from 'react-confetti';
 import './App.css';
 import {
 	CreateTodo,
@@ -7,41 +8,55 @@ import {
 	TodoList,
 	YourTask,
 } from './components';
-import Confetti from 'react-confetti';
 
-
-const defaultTodos = [
-	{ text: 'Cortar cebolla', completed: false, isDeleted: false },
-	{
-		text: 'Tomar el curso de intro a React',
-		completed: true,
-		isDeleted: false,
-	},
-	{ text: 'Llorar con la llorona', completed: false, isDeleted: false },
-	{ text: 'LALALALALALALALA', completed: false, isDeleted: false },
-];
+// const defaultTodos = [
+// 	{ text: 'Cortar cebolla', completed: false, isDeleted: false },
+// 	{
+// 		text: 'Tomar el curso de intro a React',
+// 		completed: true,
+// 		isDeleted: false,
+// 	},
+// 	{ text: 'Llorar con la llorona', completed: false, isDeleted: false },
+// 	{ text: 'LALALALALALALALA', completed: false, isDeleted: false },
+// ];
+// localStorage.setItem('todos_v1', JSON.stringify(defaultTodos));
+// LocalStorage is a key-value pair storage, it only stores strings
+// localStorage.getItem('todos_v1');
+// localStorage.removeItem('todos_v1');
 
 // This is a functional component
 function App() {
-	const [todos, setTodos] = React.useState(defaultTodos);
+	const storedTodos = JSON.parse(localStorage.getItem('todos_v1')) || [];
+	if (storedTodos.length === 0) {
+		console.log('No tasks found in localStorage');
+	}
+
+	// define todos as useState to keep track of the state
+	const [todos, setTodos] = React.useState(storedTodos);
 	const [completed, setCompleted] = React.useState(
-		defaultTodos.filter(todo => todo.completed).length
+		storedTodos.filter(todo => todo.completed).length
 	);
-	const [total, setTotal] = React.useState(defaultTodos.length);
-  const [showConfetti, setShowConfetti] = React.useState(false);
+	const [total, setTotal] = React.useState(storedTodos.length);
+	const [showConfetti, setShowConfetti] = React.useState(false);
 
+	// Update localStorage whenever todos state changes
+	React.useEffect(() => {
+		localStorage.setItem('todos_v1', JSON.stringify(todos));
+	}, [todos]);
+  // useEffect is a hook that allows you to perform side effects in your function components
+  // useEffect detects changes in the dependencies array and runs the function whenever a change is detected
 
-  const deleteTask = text => {
-    const newTodos = todos.map(todo =>
-      todo.text === text ? { ...todo, isDeleted: true } : todo
-    );
+	const deleteTask = text => {
+		const newTodos = todos.map(todo =>
+			todo.text === text ? { ...todo, isDeleted: true } : todo
+		);
 
-    const filteredTodos = newTodos.filter(todo => !todo.isDeleted);
+		const filteredTodos = newTodos.filter(todo => !todo.isDeleted);
 
-    setTodos(filteredTodos);
-    setTotal(filteredTodos.length);
-    setCompleted(filteredTodos.filter(todo => todo.completed).length);
-  };
+		setTodos(filteredTodos);
+		setTotal(filteredTodos.length);
+		setCompleted(filteredTodos.filter(todo => todo.completed).length);
+	};
 
 	const addTask = text => {
 		const newTodos = [...todos, { text, completed: false }];
@@ -57,19 +72,19 @@ function App() {
 		setTodos(newTodos);
 		setCompleted(newTodos.filter(todo => todo.completed).length);
 
-    // if all tasks are completed, then show confetti
-    if (newTodos.length === newTodos.filter(todo => todo.completed).length) {
-      console.log('All tasks are completed');
-      setShowConfetti(true);
-      setTimeout(() => setShowConfetti(false), 5000);
-    }
+		// if all tasks are completed, then show confetti
+		if (newTodos.length === newTodos.filter(todo => todo.completed).length) {
+			console.log('All tasks are completed');
+			setShowConfetti(true);
+			setTimeout(() => setShowConfetti(false), 5000);
+		}
 	};
 
 	// for searching the task in the list
 	const searchTasks = text => {
 		const filteredTodos =
 			text === ''
-				? defaultTodos
+				? storedTodos
 				: todos.filter(todo =>
 						todo.text.toLowerCase().includes(text.toLowerCase())
 					);
@@ -91,7 +106,7 @@ function App() {
 				/>
 
 				<TodoList>
-          {showConfetti && <Confetti />}
+					{showConfetti && <Confetti />}
 					{todos.map(todo => (
 						<TodoItem
 							key={todo.text}
@@ -99,7 +114,7 @@ function App() {
 							completed={todo.completed}
 							className='todo-item'
 							completeTask={completeTask}
-              deleteTask={deleteTask}
+							deleteTask={deleteTask}
 						/>
 					))}
 				</TodoList>
